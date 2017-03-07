a53a099d57617b7d810f9370ac25b806fcd9beb5
root <root@pims.westcloud>
2017-03-07 19:27:19 +0000

    Updating imports
    Signed-off-by: root <root@pims.westcloud>

--- syzygyauthenticator/google.py
+++ syzygyauthenticator/google.py
@@ -1,30 +1,41 @@
 from .syzygy import SyzygyAuthenticator
 from oauthenticator import GoogleOAuthenticator
 
-from traitlets import Integer
+from jupyterhub.handlers import BaseHandler
+
+from traitlets import Integer, Unicode
 
 from tornado import gen
 
+class SyzygyGoogleOAuthenticatorLogoutHandler(BaseHandler):
+    def get(self):
+        user = self.get_current_user()
+        if user:
+            self.log.info("User %s logged out", user.name)
+            self.clear_login_cookie()
+            for name in user.other_user_cookies:
+                self.clear_logout_cookie(name)
+            user.other_user_cookies = set([])
+            self.statsd.incr('logout')
+        self.redirect(self.authenticator.logoutURL)
+
 class SyzygyGoogleOAuthenticator(SyzygyAuthenticator, GoogleOAuthenticator):
     """A version using PAM for authentication"""
+
+    logout_handler = SyzygyGoogleOAuthenticatorLogoutHandler
 
     logoutURL = Unicode('/logout',
         help="logout URL"
     ).tag(config=True)
 
-    def SyzygyGoogleOAuthenticatorLogoutHandler(BaseHandler):
-        def get(self):
-            user = self.get_current_user()
-            if user:
-                self.log.info("User %s logged out", user.name)
-                self.clear_login_cookie()
-                for name in user.other_user_cookies:
-                    self.clear_logout_cookie(name)
-                user.other_user_cookies = set([])
-                self.statsd.incr('logout')
-            self.redirect(self.authenticator.logoutURL)
-
     def normalize_username(self, username):
         """Normalize a username"""
         username = username.lower()
         return username.split('@')[0]
+
+    def get_handlers(self, app):
+        return [
+            (r'/logout', self.logout_handler),
+            (r'/oauth_login', self.login_handler),
+            (r'/oauth_callback', self.callback_handler),
+        ]
