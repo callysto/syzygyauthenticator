a7496054a5e55e4418cf618185ff9da94a8ff9ac
root <root@striking-jaguar.callysto.farm>
2018-08-09 21:14:29 +0000

    Adding a simplified shib authenticator for launching without homedir creation
    Signed-off-by: root <root@striking-jaguar.callysto.farm>

--- syzygyauthenticator/shib.py
+++ syzygyauthenticator/shib.py
@@ -15,35 +15,13 @@ class RemoteUserLoginHandler(BaseHandler):
             self.set_login_cookie(user)
             self.redirect(self.get_next_url(user), permanent=False)
         else:
-            # c.ShibAuthenticator.shibIDAttribute   <- user@place style username
-            # c.ShibAuthenticator.shibValidEntitlements  <- entitlements considered "valid"
-            # c.ShibAuthenticator.shibUserEntitlements<- current user's attributes
-            username = self.authenticator.normalize_username(
-                self.request.headers.get(self.authenticator.shibIDAttribute, ''))
-            userEntitlements = set(self.request.headers.get(
-                self.authenticator.shibUserEntitlements, '').split(';'))
-            validEntitlements = self.authenticator.shibValidEntitlements
+            username = self.request.headers.get(self.authenticator.shibIDAttribute, '')
 
-            # Set intersection, any userentitlement in validEntitlements is enough
-            entitlement = userEntitlements & validEntitlements
+            self.log.info("User %s authorized", username)
 
-            if entitlement:
-                self.log.info("User %s authorized with entitlement %s in %s",
-                              username, entitlement, validEntitlements)
-                user = self.user_from_username(username)
-                self.set_login_cookie(user)
-                self.redirect(self.get_next_url(user), permanent=False)
-
-            else:
-                self.log.info("Missing entitlement user (%s): '%s' not in '%s')", 
-                              username, userEntitlements, validEntitlements)
-                raise web.HTTPError(403, """You have authenticated successfully to your institution, but
-                                you do not currently have the entitlements needed to access this
-                                application.  Please contact jupyter@pims.math.ca to enquire about
-                                access.""")
-
-            auth_instant = self.request.headers.get(self.authenticator.shibAuthInstant, '')
-            self.log.info("Authentication instant: %s", auth_instant)
+            user = self.user_from_username(username)
+            self.set_login_cookie(user)
+            self.redirect(self.get_next_url(user), permanent=False)
 
 
 class RemoteUserLogoutHandler(BaseHandler):
@@ -62,30 +40,12 @@ class RemoteUserAuthenticator(SyzygyAuthenticator):
     """
     Accept the authenticated user attribute from the shibboleth headers.
     """
-    shibIDAttribute = Unicode('REMOTE-USER',
+    shibIDAttribute = Unicode('REMOTE_USER',
         help="HTTP header to inspect for the authenticated username."
-    ).tag(config=True)
-    shibUserEntitlements = Unicode('eduPersonEntitlement',
-        help="HTTP header containing user's entitlements."
-    ).tag(config=True)
-    shibValidEntitlements = Set(
-        help="set containing entitlements sufficient to allow access"
     ).tag(config=True)
     shibLogoutURL = Unicode('/Shibboleth.sso/Logout',
         help="Shibboleth logout handler."
     ).tag(config=True)
-    shibAuthInstant = Unicode('Shib-Authentication-Instant',
-        help="HTTP header containing ISO timestamp of authentication instant at IdP"
-    ).tag(config=True)
-
-    def normalize_username(self, username):
-        """Normalize the username - just toss away everything after @ and cast
-        to lowercase"""
-        username = username.split('@')[0]
-        username = username.lower()
-        username = username.replace('.','-')
-        username = self.username_map.get(username, username)
-        return username
 
     def get_handlers(self, app):
         return default_handlers
